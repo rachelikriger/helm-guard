8eb06672c11ca9aa16618e65fe3d6ee374846391
Racheli Kriger <rachelikriger@gmail.com>
2026-02-19 16:50:31 +0200

    Remove gettext dependency and use busybox awk in entrypoint to avoid apk timeout when building on OpenShift

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
-# Substitute only GITLAB_PROXY_TOKEN to avoid breaking nginx $variables
-envsubst '${GITLAB_PROXY_TOKEN}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Substitute only GITLAB_PROXY_TOKEN to avoid breaking nginx $variables (awk from busybox, no gettext needed)
+awk -v t="${GITLAB_PROXY_TOKEN}" '{gsub(/\$\{GITLAB_PROXY_TOKEN\}/, t); print}' \
+  /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
